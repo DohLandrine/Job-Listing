d7a1dfd8f042854b0112e3ed5ccd668c7fe596de
Doh Landrine <landrinedoh22@gmail.com>
2025-06-07 03:09:12 +0100

    Add candidate API routes for creating and deleting candidates

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const port = 5000;
 const mongoose = require('mongoose');
 
 const jobRoutes = require('./routes/job_listings_api');
+const candidateRoutes = require('./routes/candidate_api');
 
 mongoose.connect('mongodb://localhost:27017/jobBoard');
 mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 
 app.use('/api/job_listings', jobRoutes);
+app.use('/api/candidates', candidateRoutes);
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
---
+++ routes/candidate_api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const CandidateModel = require('../models/candidate');
+const router = express.Router();
+
+// create a candidate   
+router.post('/create', (req, res, next) => {
+    const candidate = req.body;
+    CandidateModel.create(candidate)
+        .then((candidate) => {
+            res.json({ message: 'Candidate created successfully', candidate: candidate });
+        })
+        .catch(next);
+});
+
+// deleting a candidate
+router.delete('/delete/:id', (req, res, next) => {
+    const candidateId = req.params.id;
+    CandidateModel.findByIdAndDelete(candidateId)
+        .then((deletedCandidate) => {
+            if (!deletedCandidate) {
+                return res.status(404).json({ message: 'Candidate not found' });
+            }
+            res.json({ message: 'Candidate deleted successfully', candidate: deletedCandidate });
+        })
+        .catch(next);
+});
+
+module.exports = router;
